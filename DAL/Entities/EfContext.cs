d5b36b097228ef1b28c4fd9aa90af5779d00bcc9
DimaTsesyk <merlinultra@gmail.com>
2017-01-17 19:03:48 +0200

    MFirst Commit

---
+++ DAL/Entities/EfContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class EfContext:DbContext
+    {
+        public EfContext()
+            : base("MyAzureDb")
+        {
+                
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ DAL/Migrations/201701171657223_Create Database.cs
@@ -0,0 +1,26 @@
+namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
