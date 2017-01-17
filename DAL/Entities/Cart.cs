ae305250522e27717393882fd937d0fb4c47c69c
Kostetskiy <ya.kosteckiy@gmail.com>
2017-01-17 19:39:36 +0200

    added few model

---
+++ DAL/Entities/Cart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime DateCreate { get; set; }
+
+        public int UserId { get; set; }
+    
+        public User User { get; set; }
+
+        public virtual IEnumerable<CartItem> Items { get; set; }
+    
+    }
+}
