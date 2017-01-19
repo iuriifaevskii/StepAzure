68d8dfa5e747dab7a228a0f1f95c91b7dbeb792a
Novak Vova <novakvova@gmail.com>
2017-01-19 19:48:55 +0200

    Craate UserProvider

---
+++ DAL/Concrete/UserProvider.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+
+namespace DAL.Concrete
+{
+    public class UserProvider : IUserProvider
+    {
+        EfContext mContext;
+        public UserProvider()
+        {
+            mContext = new EfContext();
+        }
+        public User CreateUser(string email, string password)
+        {
+            User user = new User
+            {
+                Username=email,
+                Password=password
+            };
+            mContext.Users.Add(user);
+            mContext.SaveChanges();
+            return user;
+        }
+    }
+}
