6bff1d06ca474e22d663d0845042389554201479
Iurii Faevskii <urfaevskii@gmail.com>
2017-01-23 20:58:38 +0200

    add role in db

--- DAL/Concrete/UserProvider.cs
+++ DAL/Concrete/UserProvider.cs
@@ -1,19 +1,18 @@
 ﻿using DAL.Abstract;
-<<<<<<< HEAD
 using DAL.Entities;
-=======
->>>>>>> c21f9b55fe1a2cc3fae3a068b96563554a528dac
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DAL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-<<<<<<< HEAD
-=======
 using DAL.Entities;
 using System.Diagnostics;
 using System.Threading;
->>>>>>> c21f9b55fe1a2cc3fae3a068b96563554a528dac
 
 namespace DAL.Concrete
 {
@@ -25,22 +24,7 @@ namespace DAL.Concrete
             mContext = new EfContext();
         }
 
-<<<<<<< HEAD
-        public User CreateUser(string email, string password)
-        {
-            User user = new User
-            {
-                Username = email,
-                Password = password
-
-            };
-            mContext.Users.Add(user);
-            mContext.SaveChanges();
-
-            return user;
-        }
-
-=======
+        
         public int CountUsers
         {
             get
@@ -71,6 +55,5 @@ namespace DAL.Concrete
         {
             return mContext.Users;
         }
->>>>>>> c21f9b55fe1a2cc3fae3a068b96563554a528dac
     }
 }
